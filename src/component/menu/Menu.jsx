d0bcc5bef279f67a6890ecf2ab8edd6c6b9a6dca
whl940 <noah.wong@ha.org.hk>
2023-09-06 17:26:42 +0800

    update v1.1

--- src/component/menu/Menu.jsx
+++ src/component/menu/Menu.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import styles from './menu.module.css'
+import Image from 'next/image'
 
 const Menu = () => {
     return (
         <div className={styles.container}>
             <h2>{"What's hot"}</h2>
             <h1>Most Popular</h1>
+            <div className={styles.items}>
+                <Link href="/" className={styles.items}>
+                    <div className={styles.imageContainer}>
+                        <Image src="p1.jepg" alt="" fill className={styles.image}/>
+                    </div>
+                    <div className={styles.textContainer}></div>
+
+                </Link>
+            </div>
         </div>
     )
 }
